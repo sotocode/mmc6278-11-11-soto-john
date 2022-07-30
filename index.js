4b53407d8d51e7ea419e8eed3565bfd4b97faa65
sotocode <sotocreate@gmail.com>
2022-07-30 19:49:23 -0400

    added js code

--- index.js
+++ index.js
@@ -9,7 +9,25 @@ const pipe = (...fns) => firstArg => fns.reduce((returnValue, fn) => fn(returnVa
 const makeTag = tag => str => `<${tag}>${str}</${tag}>`
 
 // complete this function
-const makePoemHTML = () => {}
+const makePoemHTML = ([{title, author, lines}]) => {poem = []
+  poemTitle = makeTag(`h2`)(title)
+  authorName = pipe(makeTag(`em`), makeTag(`h3`))(`by ${author}`)
+  stanza = []
+  lines.forEach((text, index) => {
+    if(!text){
+      poem.push(stanza)
+      stanza = []
+    }else if(index === lines.length - 1){
+      stanza.push(text)
+      poem.push(stanza)
+    }else{
+      stanza.push(text)
+    }
+  })
+  group = ``
+  poem.forEach((stanza) => {group += makeTag(`p`)(stanza.join(`<br>`))})
+  return `${poemTitle}${authorName}${group}`
+}
 
 // attach a click event to #get-poem
 getPoemBtn.onclick = async function() {
